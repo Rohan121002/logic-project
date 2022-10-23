0bacc5cdcf2f72bc508c3a90bb5d71e13587cc90
Rohan121002 <f20212739@hyderabad.bits-pilani.ac.in>
2022-10-23 09:43:14 +0530

    added comments to question 1

--- CSS/q1.c
+++ CSS/q1.c
@@ -4,35 +4,87 @@
 # define Max 50
 int top = -1;
 char arr[Max];
-
+/**
+ * @brief Checks weather the array is full.
+ * @details This function checks weather the array is full or not if array is full than the topmost term will become max-1.
+ * 
+ * @return int 
+ */
 int isFull() {
     return top == Max - 1;
 } 
-
+/**
+ * @brief Checks weather the array is empty or not 
+ * @details It returns -1 as the topmost element 
+ * 
+ * @return int 
+ */
 int isEmpty() { 
     return top == -1; 
 }
-
+/**
+ * @brief  Creating a stack function named push().
+ * @details This is a function which puts the item to the top of the array. 
+ * @details It only does so when the array is not full.
+ * 
+ * @param item 
+ */
 void push(char item) {
     if (isFull()) 
         return; 
 	top++;
 	arr[top] = item;
 }
+
+/**
+ * @brief Removes the topmost element
+ * @details Here if the function is empty this function returns 0 as there is no element to pop
+ * @details but if its not empty than this function removes a element from the top and returns an array with size which is one less than the size of previous array.
+ * 
+ * @return int 
+ */
 int pop() { 
     if (isEmpty()) 
         return 0; 
            
     return arr[top--]; 
 } 
+
+/**
+ * @brief Prints top element in array
+ * @details If the array is not empty then it returns the top element in an array otherwise it returns 0
+ * @details It checks weather the array is not empty using isEmpty fucntion already declared
+ * @return int 
+ */
 int print(){ 
     if (isEmpty()) 
         return 0; 
     return arr[top]; 
 } 
+/**
+ * @brief Checks operand
+ * @details This is a function which checks weather the element given is operand (>,+,*,~) or not
+ * @details This gives output 1 when ch is not operand 
+ * @param ch 
+ * @return int 
+ */
 int checkIfOperand(char ch) {
     return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'); 
 } 
+
+/**
+ * @brief Checks the precedence of the operands
+ * @details This function uses switch case to give precedence order to all the operands. 
+ * @details Here the precedence order followed is (>) >> (+) >> (*) >> (~).
+ * @details It returns 1 when operand is (>)
+ * @details It returns 2 when operand is (+)
+ * @details It returns 3 when operand is (*)
+ * @details It returns 4 when operand is (~)
+ * @details It returns -1 when the element is not an operand.
+ * 
+ * @param ch 
+ * @return int 
+ */
 int precedence(char ch) 
 { 
     switch (ch) 
@@ -114,6 +166,13 @@ void brackets(char* ptr){
         i++;
     }
 }
+
+/**
+ * @brief This function converts the given expression from infix to prefix.
+ * @details It converts the expression from infix to prefix using reverse, brackets, Postfix, fucntions
+ * 
+ * @param ptr 
+ */
 void InfixtoPrefix(char *ptr){
 
     reverse(ptr);
